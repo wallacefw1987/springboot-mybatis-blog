ba635f1cf505909b96ce59600c7ac7702807bf0a
wallacefw1987 <wallacefw@qq.com>
2018-06-25 08:30:19 +0800

    ms:创建blog代码

---
+++ src/test/java/cn/wei/dbtest/SpringbootMybatisBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.wei.dbtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMybatisBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
