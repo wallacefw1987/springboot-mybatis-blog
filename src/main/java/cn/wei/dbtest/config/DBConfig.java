9b53f7644270e17e6ceed8caaa5fba06495b7c79
wallacefw1987 <wallacefw@qq.com>
2018-06-26 08:19:19 +0800

    添加了mybatis基本配置

---
+++ src/main/java/cn/wei/dbtest/config/DBConfig.java
@@ -0,0 +1,38 @@
+package cn.wei.dbtest.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by fanwei on 2018/6/26.
+ */
+@Configuration
+@Slf4j
+public class DBConfig {
+
+    @Value("${spring.datasource.druid.url}")
+    private String url;
+    @Value("${spring.datasource.druid.username}")
+    private String username;
+    @Value("${spring.datasource.druid.password}")
+    private String password;
+    @Value("${spring.datasource.druid.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    @Primary
+    public DataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+        return dataSource;
+    }
+}
